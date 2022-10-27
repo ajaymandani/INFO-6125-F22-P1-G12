61b826b0bd766d1158bc4d158fd9c5821fab87db
Nihas <nihas19990505@gmail.com>
2022-10-27 19:52:19 -0400

    added the outlets

--- Wordle/ViewController.swift
+++ Wordle/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelViewCollection: [UIView]!
+    
+    @IBOutlet var labels: [UILabel]!
+    
+    @IBOutlet weak var backBTN: UIButton!
+    
+    @IBOutlet weak var backBtnView: UIView!
+    @IBOutlet weak var submitBTN: UIButton!
+    @IBOutlet var buttonViewCollection: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for labelView in labelViewCollection{
+            labelView.layer.cornerRadius = 5;
+        }
+        
+        for btnView in buttonViewCollection{
+            btnView.layer.cornerRadius = 4;
+        }
+        backBtnView.layer.cornerRadius = 4;
+        startGame()
+    }
+    
+    func startGame()
+    {
+        backBTN.isEnabled = false
+        submitBTN.isEnabled = false
     }
 
+    
+    @IBAction func keysPressed(_ sender: UIButton) {
+        
+        print(sender.titleLabel?.text)
+    }
+    
 
 }
 
